a227ec68bde3474c644946e0e4c326b68bf4a447
peacekeeps <app.juliog@gmail.com>
2022-12-12 21:31:54 -0300

    eu sou lixo

--- 007 - condições/exerciseB/script.js
+++ 007 - condições/exerciseB/script.js
@@ -1,10 +1,28 @@
 function checkData() {
     const D = new Date()
     let anoAtual = D.getFullYear()
-    let anoNasc = Number(document.getElementById('ip_ano-nasc').value)
-    let idade = (anoAtual - anoNasc)
-    let userSex = document.getElementById('ip_sex').value
+    let anoNasc = document.getElementById('ip_ano-nasc')
+    let idade =  anoAtual - Number(anoNasc.value)
     let divResult = document.getElementById('div_resultado')
 
-    divResult.innerHTML = `Você é ${userSex}`
+    if (anoNasc.value < 1900 || anoNasc.value >= anoAtual) {
+        window.alert(`Insira um ano entre 1900 e ${anoAtual}.`)
+    } else {
+        let sexo = document.getElementsByName('ip_sex')
+        let genero = ''
+        if (sexo[0].checked) {
+            genero = 'um homem'
+            divResult.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+        } else {
+            genero = 'uma mulher'
+            divResult.innerHTML = `Detectamos ${genero} de ${idade} anos.`
+        }
+    }
 }
+
+//     if (anoNasc = 0 || anoNasc > anoAtual) {
+//         // document.getElementById('ip_ano-nasc').alert(`O ano deve estar entre 1900 e ${anoAtual}.`)
+
+//         window.alert('no')
+//     }
+// }
