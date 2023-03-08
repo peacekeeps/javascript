7e0f2567f927da9b32d8b65d9e0e0722a210412c
peacekeeps <app.juliog@gmail.com>
2023-03-07 21:43:28 -0300

    update

---
+++ Projetos Básicos de JavaScript/11-tabs/setup/app.js
@@ -0,0 +1,9 @@
+const btns = document.querySelectorAll(".tab-btn")
+
+const about = document.querySelector(".about")
+
+const articles = document.querySelectorAll(".content")
+
+about.addEventListener("click", function(e) {
+    console.log(e.target)
+})
