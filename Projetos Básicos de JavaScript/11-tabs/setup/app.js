d8b95da2ef03f1d51ffced03ad6a6bb31d48e385
peacekeeps <app.juliog@gmail.com>
2023-03-13 09:33:53 -0300

    update

--- Projetos Básicos de JavaScript/11-tabs/setup/app.js
+++ Projetos Básicos de JavaScript/11-tabs/setup/app.js
@@ -1,9 +1,24 @@
-const btns = document.querySelectorAll(".tab-btn")
+const btns = document.querySelectorAll(".tab-btn");
 
-const about = document.querySelector(".about")
+const about = document.querySelector(".about");
 
-const articles = document.querySelectorAll(".content")
+const articles = document.querySelectorAll(".content");
 
-about.addEventListener("click", function(e) {
-    console.log(e.target)
-})
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // se o elemento tiver dataset
+    // removendo a classe "active" de todos os outros botões
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+    });
+
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+      if (article.id === id) {
+        article.classList.add("active");
+      }
+    });
+  }
+});
