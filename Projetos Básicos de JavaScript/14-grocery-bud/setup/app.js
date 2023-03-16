7102f880b2ec6156ce9a80e79c96e0bba76e4970
peacekeeps <app.juliog@gmail.com>
2023-03-16 20:07:39 -0300

    grocery app

--- Projetos Básicos de JavaScript/14-grocery-bud/setup/app.js
+++ Projetos Básicos de JavaScript/14-grocery-bud/setup/app.js
@@ -1,11 +1,38 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
-
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
-
+form.addEventListener("submit", addItem);
 // ****** FUNCTIONS **********
+function addItem(e) {
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString() // pegando o tempo em milissegundos e convertendo para string, criando IDs únicas
+  if (value && !editFlag) {
+    console.log("add item to the list")
+  } else if (value && editFlag) {
+    console.log("editing")
+  } else {
+    alert.textContent = "empty value"
+    alert.classList.add("alert-danger")
+  }
+}
 
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text
+    alert.classList.add(`alert-${action}`)
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
---
+++ Projetos Básicos de JavaScript/14-grocery-bud/setup/test.js
@@ -0,0 +1,7 @@
+const test = 1
+
+if (test) {
+    console.log(test)
+} else {
+    console.log("empty")
+}
